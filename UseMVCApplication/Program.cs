eff9e3fef3988b72aae05d56ed7497b81e24fdfd
Lion1Blue <58215811+Lion1Blue@users.noreply.github.com>
2020-06-18 11:10:17 +0200

    Add files via upload

---
+++ UseMVCApplication/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using WinFormMVC.View;
+using WinFormMVC.Controller;
+
+
+namespace UseMVCApplication
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            UsersView view = new UsersView();
+            Controller controller = new Controller(view);
+            view.ShowDialog();
+        }
+    }
+}
